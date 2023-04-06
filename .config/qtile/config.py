428cdbf80627e588453db8e9ec185b6bbcde96ad
Cristobal <cristobal_jjg@hotmail.com>
2023-04-06 22:59:22 +0100

    Config

---
+++ .config/qtile/config.py
@@ -0,0 +1,149 @@
+import os
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    Key([mod, "shift"], "Left", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod, "shift"], "Right", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod, "shift"], "Down", lazy.layout.down(), desc="Move focus down"),
+    Key([mod, "shift"], "Up", lazy.layout.up(), desc="Move focus up"),
+    Key([mod, "shift"], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    
+    Key([mod], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+    
+    Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
+    
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod], "v", lazy.spawn("vivaldi"), desc="Reset all window sizes"),
+    Key([mod], "m", lazy.spawn("rofi -show drun"), desc="Reset all window sizes"),
+    Key([mod], "e", lazy.spawn("nautilus"), desc="Reset all window sizes"),
+    Key([mod], "c", lazy.spawn("code"), desc="Reset all window sizes"),
+    
+    #Control del volumen
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Bajar volumen"),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Subir volumen"),
+    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Mutear"),
+    
+    #Brillo de pantalla
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%"), desc="Subir brillo"),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-"), desc="Bajar brillo"),
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control", "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+   
+]
+
+groups = [Group(i) for i in "12345"]
+
+
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
+
+layouts = [
+    layout.Bsp(
+        border_focus="#d52e71",
+        border_normal="#64449d",
+    	border_width=2,
+        border_radius=10,
+        margin=2,
+    ),
+    layout.Max(),
+]
+
+widget_defaults = dict(
+    font="CaskaydiaCove Nerd Font Mono",
+    fontsize=10,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+wmname = "LG3D"
+
+autostart = [
+    "feh --bg-fill /home/nano/Imágenes/bg.png",
+    "picom &",
+    "numlockx on &",
+    "polybar main --config=/home/nano/.config/polybar/config.ini &",
+    ]
+
+for i in autostart:
+	os.system(i)
+
